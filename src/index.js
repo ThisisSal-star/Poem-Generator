98743bd5f347c35c0afc9a37ee1b2fe4fe82f6e7
Sal Njoroge <njorogewambui32@gmail.com>
2025-03-26 10:17:52 +0300

    added js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-alert ("Welcome to the AI-Epigram")
+function generatePoem(event){
+    event.preventDefault()
+
+            new Typewriter('#poem', {
+                strings: "Hold fast to dreams",
+                autoStart: true,
+                delay: 2,
+                cursor:"",
+              }); 
+                 
+}
+
+let poemElement = document.querySelector("#poemform-generator");
+poemElement.addEventListener("click" , generatePoem)
