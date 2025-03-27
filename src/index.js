29709f5afea27610c61eea40e423ff4bf1271c86
Sal Njoroge <njorogewambui32@gmail.com>
2025-03-27 10:25:49 +0300

    added some edit

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ function generatePoem(event){
 let instructionInput = document.querySelector("#prompt")
     let apiKey = "ad031o47ac65t5df81db4b9e365cb480";
     let context = "you are an intelligent AI that knows lots of poem, generate the shortest poem on soul script emphasizing deep emotions";
-    let prompt= `generate a soul searching poem  about ${instructionInput.value}. Make sure to follow the user intruction`;
+    let prompt= `User instructions:Generate a soul searching poem  about ${instructionInput.value}. Make sure to follow the user intruction`;
     let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
     axios.get(apiUrl).then(displayPoem);
