2fa37c718d5bdc454a97397752e8659734ffe6dd
Sal Njoroge <njorogewambui32@gmail.com>
2025-03-27 10:03:56 +0300

    added axios, ip key and url (js)

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
+function displayPoem(response) {
+  new Typewriter('#poem', {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 2,
+    cursor:"",
+  });        
+}
+
 function generatePoem(event){
     event.preventDefault()
+let instructionInput = document.querySelector("#prompt")
+    let apiKey = "ad031o47ac65t5df81db4b9e365cb480";
+    let context = "you are an intelligent AI that knows lots of poem, generate the shortest poem on soul script emphasizing deep emotions";
+    let prompt= `generate a soul searching poem  about ${instructionInput.value}. Make sure to follow the user intruction`;
+    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-            new Typewriter('#poem', {
-                strings: "Hold fast to dreams",
-                autoStart: true,
-                delay: 2,
-                cursor:"",
-              });                 
+    axios.get(apiUrl).then(displayPoem);
+
+                    
 }
 
 let poemElement = document.querySelector("#poemform-generator");
