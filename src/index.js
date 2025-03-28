73294f827a6f62b9e65a118e0269a0bab7f02595
Sal Njoroge <njorogewambui32@gmail.com>
2025-03-28 09:50:36 +0300

    update on js

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ function generateQuote(event) {
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "ad031o47ac65t5df81db4b9e365cb480";
   let context =
-    "You are a quote expert and love to write short quotes. You mission is to generate a 3 line quote in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the quote. Sign the quote with '💕💕' inside a <strong> element at the end of the quote and NOT at the beginning";
+    "You are a quote expert and love to write short quotes. You mission is to generate a 3 line quote and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the quote. Sign the quote with '💕💕' inside a <strong> element at the end of the quote and NOT at the beginning";
   let prompt = `User instructions: Generate a soft and intimate quote about ${instructionsInput.value}`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
